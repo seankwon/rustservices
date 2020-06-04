cd78eb22713c69a02070af67efb9643c286c88d1
Sean Kwon <seankwn@live.com>
2020-06-04 00:07:09 -0400

    adding a route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+use actix_web::{web, App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server running on port 8088");
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(routes::index))
+    })
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
---
+++ src/routes.rs
@@ -0,0 +1,27 @@
+use actix_web::{Error, Responder, HttpRequest, HttpResponse};
+use serde::Serialize;
+use futures::future::{ready, Ready};
+use serde_json;
+
+#[derive(Serialize)]
+struct MyJSON {
+    name: &'static str,
+}
+
+impl Responder for MyJSON {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+
+        // Create response and set content type
+        ready(Ok(HttpResponse::Ok()
+            .content_type("application/json")
+            .body(body)))
+    }
+}
+
+pub async fn index() -> impl Responder {
+    MyJSON { name: "sean kwon" }
+}
